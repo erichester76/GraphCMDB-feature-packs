f58a001d7c7889c626809ad54bbec20828ab7651
Eric Hester <eric.hester@umbrella.tech>
2026-02-25 04:14:54 +0000

    shortened names for better display

--- azure_cloud_pack/config.py
+++ azure_cloud_pack/config.py
@@ -1,6 +1,6 @@
 FEATURE_PACK_CONFIG = {
     'name': 'Microsoft Azure Cloud Pack',
-    'version': '1.0.3',
+    'version': '1.0.4',
     'description': 'Feature pack for managing Microsoft Azure cloud resources.',
     'applies_to_labels': ['Azure_Subscription', 'Azure_Resource_Group', 'Azure_VM'],
     'dependencies': ['ipam_pack'],

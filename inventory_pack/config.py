1689340578770cb72fe95baf026a4e3835e6db59
Eric Hester <eric.hester@umbrella.tech>
2026-02-25 22:19:06 -0500

    added data center and inventory

---
+++ inventory_pack/config.py
@@ -0,0 +1,9 @@
+FEATURE_PACK_CONFIG = {
+    'author': 'Eric Hester',
+    'author_email': 'eric.hester@gmail.com',
+    'name': 'Inventory Pack',
+    'version': '1.0.0',
+    'applies_to_labels': ['Device', 'Device_Type'],
+    'dependencies': ['vendor_management_pack'],
+    'tabs': []
+}
